f39968ae8998ed83caedadf4aa7b83519a91310d
Andrei Shitov <sai@arenadata.io>
2025-07-23 11:30:46 +0300

    fix(detect): fix detect problem in nested dirs

--- internal/fs/fs.go
+++ internal/fs/fs.go
@@ -1,12 +1,30 @@
 package fs
 
 import (
+	"errors"
+	"io/fs"
 	"os"
 	"path/filepath"
 )
 
-// ExistsInBin checks for path/bin/exe.
-func ExistsInBin(path, exe string) bool {
-	info, err := os.Stat(filepath.Join(path, "bin", exe))
-	return err == nil && !info.IsDir()
+var errFound = errors.New("found")
+
+// ExistsInBin recursively searches under root (including root itself)
+// for any directory containing bin/exe. It returns true on first match.
+func ExistsInBin(root, exe string) bool {
+	err := filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			// propagate FS error to abort traversal
+			return err
+		}
+		if !d.IsDir() {
+			return nil
+		}
+		binExe := filepath.Join(path, "bin", exe)
+		if info, err2 := os.Stat(binExe); err2 == nil && !info.IsDir() {
+			return errFound // found, stop walk
+		}
+		return nil
+	})
+	return errors.Is(err, errFound)
 }
